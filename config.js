f7a03096167a3c815317639d2c43914e27c7c51f
TamAnjay <88712468+TamAnjay@users.noreply.github.com>
2022-01-29 10:26:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+global.owner = ['6282211224927'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = ['6285856430321','6281232373549','6281314050985','6283106414697','6285263164566','62 83838733755','6285795431803'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'faisal123',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais'
+}
+
+// Sticker WM
+global.packname = 'inc.bot'
+global.author = 'By Tamz'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
