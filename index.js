c650a12d17c3345c2abf898afcabc1931065f3ce
TamAnjay <88712468+TamAnjay@users.noreply.github.com>
2022-01-29 10:55:20 +0700

    butuh beberapa perubahan

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('YAHAHA\nTAMA', {
+CFonts.say('INC\nBOT', {
   font: 'block',
   align: 'center',
   gradient: ['red', 'cyan']
